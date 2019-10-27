78912f544a48e08723250fa62ed1fd76c1483357
ralvarez1 <ralvarez1@my.ancoraed.com>
2019-10-27 14:33:40 -0400

    First C# Checking

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatingAverage
+{
+    class Program
+    {
+        
+        static void Main(string[] args)            
+        {
+            Console.WriteLine("Enter the Players name");
+            string strName=Console.ReadLine();
+            Console.WriteLine("Enter the Number of Hits");
+            double nHits = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Enter the Number of Bats");
+            double nBats = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(strName + "'S Batting Average is: " + nHits/nBats);
+            Console.WriteLine("{0}'s Batting Average is {1}", strName, nHits/nBats);
+            Console.ReadLine();
+
+        }
+    }
+}
